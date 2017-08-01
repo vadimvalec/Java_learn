d0e8fd8a9cb4b7ef9c14394c20f0d4ac98b668e8
vadimvalec <vadimvalec@gmail.com>
2017-08-02 00:57:54 +0300

    Add finish

--- src/JobWithFile.java
+++ src/JobWithFile.java
@@ -1,10 +1,8 @@
-import java.io.File;
-
 import java.io.IOException;
 import java.util.Scanner;
 
 
-public class ReadingFromFile {
+public class JobWithFile {
 
 
     public static void main(String[] args) throws IOException {
