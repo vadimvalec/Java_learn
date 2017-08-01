f0e577ceab787dc541cf0df828aa63492ef43cc9
vadimvalec <vadimvalec@gmail.com>
2017-08-01 19:58:36 +0300

    lesson 1

--- src/ReadingFromFile.java
+++ src/ReadingFromFile.java
@@ -1,2 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadingFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = "D:" + separator + "JavaLesons" + separator + "Java_learn"+separator+"text.txt";
+
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        System.out.println(path);
+
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close();
+    }
 }
